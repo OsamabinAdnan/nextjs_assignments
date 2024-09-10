6e02aca1287ed702e3e47a2b4fdfcddcb924cf01
OsamabinAdnan <binadnanosama@gmail.com>
2024-09-10 19:25:03 +0500

    first commit hello world program

---
+++ hello_world/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home (){
+  return (
+    <div>
+      <h1 className="bg-black text-white">Hello World!</h1>
+      <p className="italic">This is my first interaction with Next.js</p>
+    </div>
+  )
+}
