b0cd978e10e7da3ba1923b0303084189d92bab3f
OsamabinAdnan <binadnanosama@gmail.com>
2024-10-21 20:32:51 +0500

    Assignment 06 Portfolio website using Figma and NextJS

--- assignment06_portfolio_website_figma_nextjs/src/app/components/ButtonNotification/page.jsx
+++ assignment06_portfolio_website_figma_nextjs/src/app/components/ButtonNotification/page.jsx
@@ -1,22 +1,16 @@
 import 'react-notifications/lib/notifications.css';
 import React from 'react'
-import {NotificationContainer, NotificationManager} from 'react-notifications'
 import { Button } from '@/components/ui/button'
 import { TbSend2 } from "react-icons/tb";
 
 
-export default function Notification() {
-  let showNotification = () => {
-    
-    NotificationManager.success ('Submitted')
-  }
 
+export default function Notification() {
+  
   return (
     <div>
-      
-      <NotificationContainer/>
-      
-      <Button type='submit' onClick={showNotification} className="bg-[#01eeff]  drop-shadow-light hover:drop-shadow-heavy hover:bg-[#01eeff] hover:text-[#151925] font-inter font-bold rounded-[20px]">
+          {/*  */}
+      <Button type='submit' className="bg-[#01eeff]  drop-shadow-light hover:drop-shadow-heavy hover:bg-[#01eeff] hover:text-[#151925] font-inter font-bold rounded-[20px]">
       <TbSend2 /> Submit
       </Button>
 
