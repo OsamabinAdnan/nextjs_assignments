1bf87eaec6e5f7c616c80813d4351178d1a92c17
OsamabinAdnan <binadnanosama@gmail.com>
2024-11-30 22:43:52 +0500

    commit 4

--- assignment12_authjs_github_google_signup_login_page/app/page.tsx
+++ assignment12_authjs_github_google_signup_login_page/app/page.tsx
@@ -10,7 +10,7 @@ import SignInPage from "@/components/login/SignInComponent";
 
 export default async function Home() {
     const session = await auth();
-    // const isAuthenticated = !!session?.user
+    const isAuthenticated = !!session?.user
   return (
     <>
       <main className=" bgmain bgmainSecond shadow-2xl bg-center bg-cover bx-shadow rounded-2xl w-9/12 h-[85%] max-h-[100%] overflow-hidden">
@@ -18,13 +18,27 @@ export default async function Home() {
         <Navbar/>
         <div className="grid grid-col-1 lg:grid-cols-3 items-start ">
           <div>
-          <Form/>
-          
-            <div className="flex flex-col justify-center items-center">
-              <SessionWrapper>
-                <SignInPage/>
-              </SessionWrapper>
-            </div>
+          {isAuthenticated ? (
+              // Content for authenticated users
+              <div>
+                <h1 className="text-2xl font-bold ml-5">Welcome, {session?.user?.name || 'User'}!</h1>
+                {/* Add authenticated user content here */}
+                <p className="text-xl font-semibold ml-5">You are logged in.</p>
+              </div>
+            ) : (
+                  // Content for unauthenticated users
+                  <div>
+                    <Form/>
+            
+                    <div className="flex flex-col justify-center items-center">
+                      <SessionWrapper>
+                      <SignInPage/>
+                      </SessionWrapper>
+                    </div>
+                  </div>
+                )
+            }
+            
           
           </div>
         </div>
